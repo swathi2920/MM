856bea2dfac53f27edcc03859ad7eaa0d0f4839a
Laxmi <reddy.laxmid@gmail.com>
2023-05-04 18:21:33 +0530

    kk

---
+++ TeamBDD/src/test/java/pay1/sample.java
@@ -0,0 +1,13 @@
+package pay1;
+
+import org.testng.annotations.Test;
+
+import pay.Demo1;
+
+public class sample {
+  @Test
+  public void f() {
+	  Demo1 d1= new Demo1();
+	  d1.display();
+  }
+}
